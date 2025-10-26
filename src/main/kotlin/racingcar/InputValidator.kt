4fce448da411ac5bb1d2803e99a8312d2757eb8e
cksckckcks <chan679999@naver.com>
2025-10-26 17:04:42 +0900

    style: 코드 컨벤션에 맞춰 공백 추가

--- src/main/kotlin/racingcar/InputValidator.kt
+++ src/main/kotlin/racingcar/InputValidator.kt
@@ -1,18 +1,17 @@
 package racingcar
 
 object InputValidator {
-    fun validateMember(memberList: List<String>){
-        memberList.map { memberName ->
-            when {
-                memberName.isEmpty() -> throw IllegalArgumentException("참가자 이름은 비어있을 수 없습니다.")
-                memberName.length > 5 -> throw IllegalArgumentException("참가자 이름은 5자 이하여야 합니다.")
-                else -> memberName
-            }
-        }
-    }
+	fun validateMember(memberList: List<String>) {
+		memberList.map { memberName ->
+			when {
+				memberName.isEmpty() -> throw IllegalArgumentException("참가자 이름은 비어있을 수 없습니다.")
+				memberName.length > 5 -> throw IllegalArgumentException("참가자 이름은 5자 이하여야 합니다.")
+				else -> memberName
+			}
+		}
+	}
 
-    fun validateTryCount(tryCount: Int) {
-        if (tryCount < 1)
-            throw IllegalArgumentException("시도 횟수는 0 이상의 정수여야 합니다.")
-    }
+	fun validateTryCount(tryCount: Int) {
+		if (tryCount < 1) throw IllegalArgumentException("시도 횟수는 0 이상의 정수여야 합니다.")
+	}
 }
