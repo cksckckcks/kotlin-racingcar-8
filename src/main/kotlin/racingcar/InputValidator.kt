258dee4c918fce7003da4953ad4cba7e43c719d7
cksckckcks <chan679999@naver.com>
2025-10-26 16:21:51 +0900

    feat: 입력받은 값을 검증하는 기능 추가

---
+++ src/main/kotlin/racingcar/InputValidator.kt
@@ -0,0 +1,18 @@
+package racingcar
+
+object InputValidator {
+    fun validateMember(memberList: List<String>){
+        val validatedMemberList = memberList.map { memberName ->
+            when {
+                memberName.isEmpty() -> throw IllegalArgumentException("참가자 이름은 비어있을 수 없습니다.")
+                memberName.length > 5 -> throw IllegalArgumentException("참가자 이름은 5자 이하여야 합니다.")
+                else -> memberName
+            }
+        }
+    }
+
+    fun validateTryCount(tryCount: Int) {
+        if (tryCount < 1)
+            throw IllegalArgumentException("시도 횟수는 0 이상의 정수여야 합니다.")
+    }
+}
