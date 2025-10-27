e4c7017f37ac5ee08f872af9120c5396bcad70b9
cksckckcks <chan679999@naver.com>
2025-10-27 16:54:11 +0900

    refactor(InputValidator): map을 forEach로 변경 및 불필요한 else 제거

--- src/main/kotlin/racingcar/InputValidator.kt
+++ src/main/kotlin/racingcar/InputValidator.kt
@@ -5,11 +5,10 @@ object InputValidator {
 	private const val MIN_TRY_COUNT = 1
 
 	fun validateMember(memberList: List<String>) {
-		memberList.map { memberName ->
+		memberList.forEach() { memberName ->
 			when {
 				memberName.isEmpty() -> throw IllegalArgumentException("참가자 이름은 비어있을 수 없습니다.")
 				memberName.length > MAX_NAME_LENGTH -> throw IllegalArgumentException("참가자 이름은 ${MAX_NAME_LENGTH}자 이하여야 합니다.")
-				else -> memberName
 			}
 		}
 	}
