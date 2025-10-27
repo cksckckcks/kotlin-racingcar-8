158bfd2d96c4e88a8bc023a0300997e8f2c6615a
cksckckcks <chan679999@naver.com>
2025-10-27 16:29:55 +0900

    style: 들여쓰기를 스페이스에서 탭으로 통일

--- src/main/kotlin/racingcar/InputParser.kt
+++ src/main/kotlin/racingcar/InputParser.kt
@@ -1,15 +1,15 @@
 package racingcar
 
 object InputParser {
-    private const val DELIMITER = ","
+	private const val DELIMITER = ","
 
-    fun parseMembers(members: String): List<String> {
-        return members.split(DELIMITER)
-    }
+	fun parseMembers(members: String): List<String> {
+		return members.split(DELIMITER)
+	}
 
-    fun parseTryCount(inputTryCount: String): Int {
-        val tryCount = inputTryCount.toIntOrNull() ?: throw IllegalArgumentException("시도 횟수는 숫자여야 합니다.")
+	fun parseTryCount(inputTryCount: String): Int {
+		val tryCount = inputTryCount.toIntOrNull() ?: throw IllegalArgumentException("시도 횟수는 숫자여야 합니다.")
 
-        return tryCount
-    }
+		return tryCount
+	}
 }
