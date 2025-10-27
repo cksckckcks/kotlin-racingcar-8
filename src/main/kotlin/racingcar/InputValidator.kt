36d78ab287bc9fe4d92a92f0d44f5c6b9836db7b
cksckckcks <chan679999@naver.com>
2025-10-27 13:47:25 +0900

    refactor(InputValidator): 매직 넘버를 상수로 변경

--- src/main/kotlin/racingcar/InputValidator.kt
+++ src/main/kotlin/racingcar/InputValidator.kt
@@ -1,17 +1,20 @@
 package racingcar
 
 object InputValidator {
+	private const val MAX_NAME_LENGTH = 5
+	private const val MIN_TRY_COUNT = 1
+
 	fun validateMember(memberList: List<String>) {
 		memberList.map { memberName ->
 			when {
 				memberName.isEmpty() -> throw IllegalArgumentException("참가자 이름은 비어있을 수 없습니다.")
-				memberName.length > 5 -> throw IllegalArgumentException("참가자 이름은 5자 이하여야 합니다.")
+				memberName.length > MAX_NAME_LENGTH -> throw IllegalArgumentException("참가자 이름은 ${MAX_NAME_LENGTH}자 이하여야 합니다.")
 				else -> memberName
 			}
 		}
 	}
 
 	fun validateTryCount(tryCount: Int) {
-		if (tryCount < 1) throw IllegalArgumentException("시도 횟수는 0 이상의 정수여야 합니다.")
+		if (tryCount < MIN_TRY_COUNT) throw IllegalArgumentException("시도 횟수는 $MIN_TRY_COUNT 이상의 정수여야 합니다.")
 	}
 }
